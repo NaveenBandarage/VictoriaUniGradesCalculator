c3714084e9eb994338c473725ae51ddf15b17c8c
Naveen Bandarage <thebanterage@gmail.com>
2020-01-30 11:38:54 +1300

    not being able to read and write in the same file atm.

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -5,9 +5,8 @@ print("These are all the courses you are taking this year.\n")
 courses = []
 grades = []
 
-file1 = open("courseListWGrades.txt","w") #opening file in append mode;
+file1 = open("courseListWGrades.txt","a") #opening file in append mode;
 file2 = open("courseList.txt","r") #This is the file from which will provide inputs
-file3 = open
 
 
 # Set new_name to something other than 'quit'.
@@ -50,8 +49,16 @@ while grade != quit:
         print("If you got more courses then feel free to add else type quit")
         file1.write(courses[grade] + gradeValue + '\n')
 
-file2.close
-print("These are your current grades:")
+file1.close
+file3 = open("courseListWGrades.txt","r+")
+
+
+line2 = file3.readline()
+print(line2)
+while line2:
+    courseLine = line2.rstrip('\n')
+    print(courseLine)
+    line2 = file3.readline()
 
 
 #    print(course)
